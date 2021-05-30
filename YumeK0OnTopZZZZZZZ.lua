95f3a7b2a1e410f779401eea666e8c167427ee6c
sakura337 <79789659+sakikuri249@users.noreply.github.com>
2021-05-30 17:18:17 +0700

    Update YumeK0OnTopZZZZZZZ.lua

--- YumeK0OnTopZZZZZZZ.lua
+++ YumeK0OnTopZZZZZZZ.lua
@@ -6,7 +6,7 @@ if game.PlaceId == 6876139165 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/BangBar%20Vibe.lua"))()
 elseif game.PlaceId == 4660690549 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/SA-RA-PUK-JAI.lua"))()
-elseif game.PlaceId == 6750972108 then 
+elseif game.PlaceId == 5388509011 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/Vibe%20NYC.lua"))()
 elseif game.PlaceId == 6750972108 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/BangBar%20Vibe.lua"))()
