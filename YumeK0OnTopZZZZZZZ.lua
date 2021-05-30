953b87761ad9dacb68661456c443a06ccf5c3df7
sakura337 <79789659+sakikuri249@users.noreply.github.com>
2021-05-30 17:11:29 +0700

    Update YumeK0OnTopZZZZZZZ.lua

--- YumeK0OnTopZZZZZZZ.lua
+++ YumeK0OnTopZZZZZZZ.lua
@@ -2,10 +2,14 @@ pcall(function()
 local market = game:GetService("MarketplaceService")
 local info = market:GetProductInfo(game.PlaceId, Enum.InfoType.Asset)
 pcall(function() end)
-if game.PlaceId == 5873526027 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/BanTai.lua"))()
+if game.PlaceId == 6876139165 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/BangBar%20Vibe.lua"))()
 elseif game.PlaceId == 4660690549 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/SA-RA-PUK-JAI.lua"))()
+elseif game.PlaceId == 6750972108 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/Vibe%20NYC.lua"))()
+elseif game.PlaceId == 6750972108 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sakikuri249/YumekoOnTopZ/main/BangBar%20Vibe.lua"))()
 else
     game.Players.LocalPlayer:Kick("GAME NOT SUPPORT This HUB !!!")
 end
